1f47e8502a150d7357cd8ed9c4bbeba0f29ada48
River Wang <riversheher@pm.me>
2024-12-12 12:04:52 -0500

    base implementation for price string conversion to dollars and eights

--- internal/asset_price.py
+++ internal/asset_price.py
@@ -11,4 +11,9 @@ def price_to_dollars_cents(asset_string: str) -> float:
 
 def price_to_dollars_eighths(asset_string: str) -> float:
     
-    return 0
+    asset_string = asset_string.strip()  # Remove leading and trailing spaces
+    
+    cents = (int(asset_string[-2:])/8)/10
+    dollars = int(asset_string[:-2])
+    
+    return dollars + cents
--- tests/test_asset_price.py
+++ tests/test_asset_price.py
@@ -1,5 +1,5 @@
 import unittest
-from internal.asset_price import price_to_dollars_cents
+from internal.asset_price import *
 
 class TestPriceToDollarsCents(unittest.TestCase):
 
@@ -13,6 +13,20 @@ class TestPriceToDollarsCents(unittest.TestCase):
         """Tests values with filler spaces as described by the BODB guide"""
         result = price_to_dollars_cents(" 5140")
         expected = 51.4
+        self.assertEqual(result, expected)
+        
+class TestPriceToDollarsEights(unittest.TestCase):
+    
+    def test_normal_value(self):
+        """Test a normal, correctly formatted asset price string."""
+        result = price_to_dollars_eighths("05140")
+        expected = 51.5
+        self.assertEqual(result, expected)
+        
+    def test_value_with_whitespaces(self):
+        """Tests values with filler spaces as described by the BODB guide"""
+        result = price_to_dollars_eighths(" 5140")
+        expected = 51.5
         self.assertEqual(result, expected)
         
 if __name__ == "__main__":
