e935ef52927c7f451c4caa580ec89febf9687981
River Wang <riversheher@pm.me>
2024-12-15 22:46:27 -0500

    stubs for basic data types and extractor componenets

--- internal/asset_price.py
+++ internal/asset_price.py
@@ -1,5 +1,15 @@
 
 def price_to_dollars_cents(asset_string: str) -> float:
+    """price_to_dollars_cents takes a string representing an asset price in the format 
+    "DDDCC" where D is a digit representing dollars and C is a digit representing cents. 
+    The function returns the asset price as a float.
+
+    Args:
+        asset_string (str): The asset price as a string in the format "DDDCC"
+
+    Returns:
+        float: The asset price as a float
+    """
     
     asset_string = asset_string.strip()  # Remove leading and trailing spaces
     
@@ -10,6 +20,15 @@ def price_to_dollars_cents(asset_string: str) -> float:
     return dollars + cents
 
 def price_to_dollars_eighths(asset_string: str) -> float:
+    """price_to_dollars_eighths takes a string representing an asset price in the format
+    "DDDC0" where D is a digit representing dollars and C is a digit representing eighths of a dollar.
+
+    Args:
+        asset_string (str): The asset price as a string in the format "DDDC0"
+
+    Returns:
+        float: the asset price as a float
+    """
     
     asset_string = asset_string.strip()  # Remove leading and trailing spaces
     
