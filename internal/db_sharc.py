4b0c3b9984a1d420476290b89fbf6418c2212813
Nomaan <NomaanTheSupreme@bitbucket.org>
2025-03-26 00:24:59 -0400

    further adjustments on the get connect:

--- internal/db_sharc.py
+++ internal/db_sharc.py
@@ -1,9 +1,13 @@
+import os
+
 import psycopg2
 
 def get_connection():
     return psycopg2.connect(
         dbname="bodb",
         user="nabbasey",
-        host="localhost",
-        port=5432
+        host=os.environ.get("PGHOST", "localhost"),
+        port=os.environ.get("PGPORT", 5432)
     )
+
+#port=5432
