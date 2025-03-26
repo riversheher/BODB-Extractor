6701eff25ad201d2bb25ddfdae0c2a250d3dd2e5
Nomaan <NomaanTheSupreme@bitbucket.org>
2025-03-26 14:56:21 -0400

    didnt pass pghost in correctly

--- internal/db_sharc.py
+++ internal/db_sharc.py
@@ -3,6 +3,11 @@ import os
 import psycopg2
 
 def get_connection():
+    if "PGHOST" not in os.environ:
+        job_id = os.environ.get("SLURM_JOB_ID")
+        if job_id:
+            os.environ["PGHOST"] = f"/home/nabbasey/scratch/pg_sock{job_id}"
+
     return psycopg2.connect(
         dbname="bodb",
         user="nabbasey",
