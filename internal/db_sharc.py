27c6f6995ed147bba6a5aef8fecc23b8cba4b6fb
Nomaan <NomaanTheSupreme@bitbucket.org>
2025-03-26 16:54:27 -0400

    doing stuff sequentially

--- internal/db_sharc.py
+++ internal/db_sharc.py
@@ -3,16 +3,11 @@ import os
 import psycopg2
 
 def get_connection():
-    if "PGHOST" not in os.environ:
-        job_id = os.environ.get("SLURM_JOB_ID")
-        if job_id:
-            os.environ["PGHOST"] = f"/home/nabbasey/scratch/pg_sock{job_id}"
-
     return psycopg2.connect(
         dbname="bodb",
         user="nabbasey",
-        host=os.environ.get("PGHOST", "/tmp"),
-        port=os.environ.get("PGPORT", 5432)
+        host="localhost",
+        port=5432
     )
 
 #port=5432
